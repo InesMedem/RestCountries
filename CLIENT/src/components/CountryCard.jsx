ea5de67fbc6e9a829377e165b663fe395db667ff
InesMedem <inesmedem93@gmail.com>
2024-08-13 10:08:47 +0200

    country card

--- CLIENT/src/components/CountryCard.jsx
+++ CLIENT/src/components/CountryCard.jsx
@@ -1,12 +1,24 @@
 const CountryCard = ({ country }) => {
   return (
-    <div>
-      <h2>{country.name.common}</h2>
-      <p>
-        Currency:
-        {country.currencies && Object.values(country.currencies)[0].name}(
-        {country.currencies && Object.values(country.currencies)[0].symbol})
-      </p>
+    <div className="card">
+      <h2>
+        {country.name.common} {country.flag}{" "}
+      </h2>
+      <h4> ({country.name.official})</h4>
+      {/* <img src={country.flag.svg} /> */}
+      <div>
+        <ul>
+          <li>
+            Currency:
+            {country.currencies && Object.values(country.currencies)[0].name}(
+            {country.currencies && Object.values(country.currencies)[0].symbol})
+          </li>
+          <li>Capital: {country.capital} </li>
+          <li>Region: {country.region} </li>
+          <li>Area: {country.area} km²</li>
+          <li>Population {country.population} </li>
+        </ul>
+      </div>
     </div>
   );
 };
